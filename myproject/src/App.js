06c388a53c5fd0d9a7a583c412daf5426a990385
Haroon <biodunhi@gmail.com>
2024-05-17 03:51:27 +0500

    first commit

---
+++ myproject/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Person from "./Components/person";
+function App() {
+  return (
+    <div className="App">
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+    </div>
+  );
+}
+export default App;
+ 
---
+++ myproject/src/Components/person.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Person extends Component {
+  render() {
+    return (
+      <div>
+        <div>Name: Haroon nadir</div>
+        <div>Age: 21 years</div>
+      </div>
+    );
+  }
+}
+
+export default Person;
